02f0f564368c2f43e839f7d2afad187bd7f2526d
Kathryn Young <kathryn.young@neueda.com>
2020-01-21 15:55:08 +0000

    Controller to ControllerBase

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -9,16 +9,12 @@ namespace StarChart.Controllers
 {
     [Route("")]
     [ApiController]
-    public class CelestialObjectController : Controller
+    public class CelestialObjectController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
 
         private readonly ApplicationDbContext _context;
         public CelestialObjectController(ApplicationDbContext context)
-        {
+        { 
             _context = context;
         }
     }
